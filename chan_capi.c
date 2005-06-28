580263f9ceb04dda24867cd11bd4084a2c269d2c
MelwareDE <armin@melware.de>
2005-06-28 19:26:56 +0000

    Use CVS Revision in version string.

--- chan_capi.c
+++ chan_capi.c
@@ -52,7 +52,8 @@
 #include "chan_capi_app.h"
 #include "chan_capi_pvt.h"
 
-#define CC_VERSION "cm-0.6dev4"
+/* #define CC_VERSION "cm-x.y.z" */
+#define CC_VERSION "$Revision$"
 
 #ifdef CAPI_ULAW
 #define LAW_STRING "uLaw"
